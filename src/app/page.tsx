08eaa7cc8039821888dcd8f71266e4abdbae234c
ahujaankush <aahuja06@proton.me>
2024-10-12 22:16:49 +0200

    Add responsiveness to /home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 import { ArrowRight } from 'lucide-react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Separator } from "@/components/ui/separator";
-import { FileText, Search, Copy, Star } from 'lucide-react';
+import { FileText, Search, Copy } from 'lucide-react';
 import Image from "next/image";
 import editorImage from "@/../public/homepage/editor/Editor.png"
 import tracerImage from "@/../public/homepage/editor/Tracer.png"
@@ -10,64 +10,65 @@ import workflowsImage from "@/../public/homepage/editor/Workflows.png"
 
 export default function Home() {
   return (
-    <main className="text-center min-h-screen bg-gradient-to-b bg-fixed from-gradient-start from-30% to-gradient-end to-70% space-y-16">
-      <section className="space-y-8">
-        <h1 className="justify-center md:text-8xl md:px-40 pt-40">
+    <main className="w-full text-center min-h-screen bg-gradient-to-b bg-fixed from-gradient-start to-gradient-end space-y-16">
+      <section className="space-y-8 px-12 lg:px-44">
+        <h1 className="justify-center text-5xl md:text-8xl pt-40">
           <span className="font-bold">Track</span>,
           <span className="font-bold"> Access</span>,
           and
           <span className="font-bold"> Process </span>
           all your data on<span className="text-blue-300"> one platform</span>
         </h1>
-
-        <p className="text-center justify-center text-xl md:text-3xl md:px-96">
+        <p className="text-center justify-center text-xl md:text-3xl">
           Open-Source Automation & Data Centralization Platform
           <br />
           Scalability is more than a buzzword to us
         </p>
 
-        <Button className="text-3xl rounded-lg border-4 border-blue-500 bg-transparent hover:bg-transparent text-primary px-4 py-8 space-x-2">
+        <Button className="text-xl rounded-lg border-4 border-blue-500 bg-transparent hover:bg-transparent text-primary px-3 py-6 space-x-2">
           <span>Get Kayf Now</span>
-          <ArrowRight size={40} />
+          <ArrowRight size={30} />
         </Button>
       </section>
 
       <section className="flex grow flex-col items-center">
         <Tabs defaultValue="tracer" className="flex flex-col align-center">
-          <TabsList className="flex flex-row space-x-28 bg-transparent">
-            {
-              [
-                {
-                  key: "editor",
-                  text: "Editor",
-                  icon: <FileText size={40} className="mr-1 md:mr-2" />
-                },
-                {
-                  key: "tracer",
-                  text: "Tracer",
-                  icon: <Search size={40} className="mr-1 md:mr-2" />
-                },
-                {
-                  key: "workflows",
-                  text: "Workflows",
-                  icon: <Copy size={40} className="mr-1 md:mr-2" />
-                }
-              ].map((e, i) => (
-                <TabsTrigger key={`content-preview-tab-${i}`} value={e.key} className="duration-100 md:text-4xl rounded-none pb-4 border-b-4 border-transparent data-[state=active]:border-blue-800 data-[state=active]:text-blue-800 bg-transparent hover:bg-transparent data-[state=active]:bg-transparent">
-                  {e.icon}
-                  <span>{e.text}</span>
-                </TabsTrigger>
-              ))
-            }
+          <TabsList className="flex flex-col bg-transparent mb-[2rem]">
+            <div className="flex flex-row space-x-6 md:space-x-12 lg:space-x-24">
+              {
+                [
+                  {
+                    key: "editor",
+                    text: "Editor",
+                    icon: <FileText className="w-6 h-6 lg:w-8 lg:h-8" />
+                  },
+                  {
+                    key: "tracer",
+                    text: "Tracer",
+                    icon: <Search className="w-6 h-6 lg:w-8 lg:h-8" />
+                  },
+                  {
+                    key: "workflows",
+                    text: "Workflows",
+                    icon: <Copy className="w-6 h-6 lg:w-8 lg:h-8" />
+                  }
+                ].map((e, i) => (
+                  <TabsTrigger key={`content-preview-tab-${i}`} value={e.key} className="rounded-none pb-4 border-b-4 border-transparent data-[state=active]:border-blue-800 data-[state=active]:text-blue-800 bg-transparent hover:bg-transparent data-[state=active]:bg-transparent">
+                    {e.icon}
+                    <span className="hidden md:inline md:ml-2 text-2xl lg:text-3xl">{e.text}</span>
+                  </TabsTrigger>
+                ))
+              }
+            </div>
+            <Separator className="bg-blue-800 rounded-full opacity-25 mt-[-4px] h-1 self-center w-[75%]" />
           </TabsList>
-          <Separator className="bg-blue-800 rounded-full opacity-25 h-1 mt-2 mb-4 w-[65%] self-center" />
-          <TabsContent value="editor" className="self-center w-[75%]">
+          <TabsContent value="editor" className="self-center w-[85%]">
             <Image src={editorImage} alt="Editor" className="rounded" />
           </TabsContent>
-          <TabsContent value="tracer" className="center self-center w-[75%]">
+          <TabsContent value="tracer" className="center self-center w-[85%]">
             <Image src={tracerImage} alt="Editor" className="rounded" />
           </TabsContent>
-          <TabsContent value="workflows" className="self-center w-[75%]">
+          <TabsContent value="workflows" className="self-center w-[85%]">
             <Image src={workflowsImage} alt="Editor" className="rounded" />
           </TabsContent>
         </Tabs>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,9 +2,10 @@
 import { MenuIcon, Star } from "lucide-react";
 import Link, { LinkProps } from "next/link";
 import { Button } from "../ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
 import Image from "next/image";
 import logo from "@/../public/logo.svg"
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 export function LinkHoverAnim({ children, className = '', ...props }: LinkProps & { children: React.ReactNode, className?: string }) {
   return (
@@ -19,7 +20,7 @@ export function LinkHoverAnim({ children, className = '', ...props }: LinkProps 
 
 export default function Header() {
   return (
-    <div className="fixed w-full flex flex-row grow border-b-2 px-6 py-4 backdrop-blur-sm">
+    <div className="fixed w-full flex flex-row grow border-b-2 px-6 py-4 backdrop-blur-sm bg-primary-foreground/25">
       <div className="flex justify-start">
         <Link href="#" className="flex items-center" prefetch={false}>
           <Image src={logo} alt="Logo" height={40} className="absolute invert" />
@@ -53,16 +54,23 @@ export default function Header() {
             </Button>
           </SheetTrigger>
           <SheetContent side="top">
-            <div className="flex flex-col space-y-4">
-              <Link href="#" className="flex items-center" prefetch={false}>
-                <Image src={logo} alt="Logo" height={40} className="absolute invert" />
-                <span className="text-3xl pl-14">Kayf</span>
-              </Link>
+            <SheetHeader className="flex flex-row justify-content-center">
+              <Image src={logo} alt="Logo" height={40} className="absolute invert" />
+              <SheetTitle className="text-3xl pl-14 text-left font-normal">
+                Kayf
+              </SheetTitle>
+              <VisuallyHidden>
+                <SheetDescription>
+                  Navigation Menu
+                </SheetDescription>
+              </VisuallyHidden>
+            </SheetHeader>
+            <div className="flex flex-col space-y-4 mt-4">
               {
                 ['Product', 'Download', 'Pricing', 'Team', 'Blog'].map((e, i) => (
-                  <Link href="#" key={`navigation-mobile-item-${i}`} className="text-2xl font-medium hover:underline underline-offset-4" prefetch={false}>
+                  <LinkHoverAnim href="#" key={`navigation-mobile-item-${i}`} className="text-2xl" prefetch={false}>
                     {e}
-                  </Link>
+                  </LinkHoverAnim>
                 ))
               }
               <Link href="https://github.com/KayfApp">
