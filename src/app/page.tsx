e7523c529559bbda692c5d8dc3f29c0aa8e701ad
pbakshi05 <pbakshi@student.tgm.ac.at>
2024-12-01 00:58:16 +0100

    Homepage Responsive gemacht

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
     const { resolvedTheme } = useTheme()
     return (
         <Main>
-            <Section className="h-screen !items-start">
-                <div className="h-full w-[65vw] flex flex-col justify-end space-y-4">
+            <Section className="lg:h-screen !items-start">
+                <div className="h-full w-[65vw] flex flex-col justify-start mt-[140px] lg:mt-0 lg:justify-end space-y-4 !pb-0">
                     <Heading1 className="lg:text-7xl font-semibold">
                         Track, Access, and Process all your data on one platform.
                     </Heading1>
@@ -35,14 +35,14 @@ export default function Home() {
             </Section>
             <Separator />
             <Section className="flex flex-col grow justify-center text-center space-y-14">
-                <Heading2 className="lg:text-5xl border-b-0 line leading-loose">
+                <Heading2 className="text-[17px] lg:text-5xl border-b-0 line leading-loose">
                     Kayf is a <u>hyper-extendable</u> editor with scalable <br /><u>enterprise search and automation platform</u>,
                     <br />
                     fully tailored to your requirements.
                     <br />
                     <b>Powerful, private, and tracking-free.</b>
                 </Heading2>
-                <div className="flex flex-col w-[90vw] text-left space-y-24">
+                <div className="hidden lg:block flex flex-col w-[90vw] text-left space-y-24">
                     {
                         [
                             {
@@ -69,9 +69,36 @@ export default function Home() {
                         })
                     }
                 </div>
+                <div className="lg:hidden flex flex-col w-[90vw] text-left space-y-24">
+                    {
+                        [
+                            {
+                                title: "Why?",
+                                text: <>Many organizations fail to fully leverage social technologies, limiting their productivity. <Link className="underline" href={"https://www.mckinsey.com/~/media/mckinsey/industries/technology%20media%20and%20telecommunications/high%20tech/our%20insights/the%20social%20economy/mgi_the_social_economy_full_report.pdf"}>The McKinsey Global Institute</Link> estimates that companies could boost knowledge worker productivity by 20 to 25% by improving internal communication. With significant time spent managing emails and searching for information, there's a critical need for solutions that streamline data management and enhance collaboration.</>
+                            },
+                            {
+                                title: "How?",
+                                text: "Centralizing organizational information on a single platform makes it easier to manage and access. By streamlining search capabilities and automating repetitive tasks, efficiency is enhanced and time spent on information retrieval is reduced. This solution fosters better communication and collaboration within teams, empowering them to work smarter and more effectively."
+                            },
+                            {
+                                title: "When?",
+                                text: <>This project is part of a diploma thesis, with the first prototype expected in November 2024. The code will be accessible on GitHub throughout the development process. Research results are planned for release in April 2025, with additional details available in our <Link className="underline" href='/progress'>milestone plan</Link>.</>
+                            },
+                        ].map((e, i) => {
+                            return (<div key={`${i}-homepage-product-features`} className="flex flex-col grow items-center rounded-lg p-10 border-2">
+                                <Large className="text-5xl font-normal pb-10">
+                                    {e.title}
+                                </Large>
+                                <div className="flex grow justify-center">
+                                    <Paragraph className="w-full justify-center leading-loose">{e.text}</Paragraph>
+                                </div>
+                            </div>)
+                        })
+                    }
+                </div>
             </Section>
             <Separator />
-            <Section className="space-y-48">
+            <Section className="hidden lg:block space-y-48">
                 {
                     [
                         {
@@ -128,17 +155,74 @@ export default function Home() {
                     })
                 }
             </Section>
+            <Section className="lg:hidden space-y-48">
+                {
+                    [
+                        {
+                            title: 'Note-taking and collaboration simplified', text: <>KEditor is where note-taking meets creativity in the coolest way possible! With two modes to match your flow - a sleek, polished <span className="font-bold">Writing Mode</span> for structured notes and a limitless <span className="font-bold">Canvas Mode</span> that turns your workspace into a whiteboard - your ideas can go from organized to out-of-the-box in a snap. Seamless integration between these modes means you can switch gears without missing a beat.</>,
+                            img: editorImage
+                        },
+                        {
+                            title: 'Meet Tracer, the ultimate enterprise search', text: <>Tracer lets you search across multiple channels - like Email, Teams, and more—all at once. It doesn&apos;t just skim the surface; it understands your query&apos;s actual meaning to deliver the most relevant results, using your data securely without ever collecting any.
+                                <br />
+                                <span className="font-bold">And here&apos;s the game-changer</span>: You can actually ask an chat bot questions directly about your data! Whether you need quick insights, or detailed breakdowns, the model gets you the answers instantly - no more digging through endless files and threads.
+                            </>,
+                            img: tracerImage
+                        },
+                        {
+                            title: 'Simplify tasks and easily automate workflows', text: <>Workflows are small, repetitive tasks in organizations that often go unautomated due to the hassle. We&apos;ve made saving time easier by integrating popular workflow services and enhancing them with powerful AI tools!
+                                <br />
+                                <span className="font-bold">And here&apos;s the best part</span>: You can access data directly from the editor, allowing you to process information automatically in real-time. Plus, by integrating with hundreds of additional services, you can solve tasks more efficiently and streamline operations like never before.</>, img: workflowsImage
+                        }
+                    ].map((e, i) => {
+                        return (
+                            <div className={`flex flex-col space-x-28`}>
+                                {
+                                    (i % 2 == 0) ?
+                                        (
+                                            <>
+                                                <div className="flex flex-col self-center leading-snug w-full">
+                                                    <Heading3 className="lg:text-4xl font-normal pb-8">
+                                                        {e.title}
+                                                    </Heading3>
+                                                    <Image src={e.img} alt="Tracer demo" className="rounded-2xl w-full drop-shadow-lg" />
+                                                    <Paragraph className="leading-loose">
+                                                        {e.text}
+                                                    </Paragraph>
+                                                </div>
+                                            </>
+                                        )
+                                        :
+                                        (
+                                            <>
+                                                <div className="flex flex-col self-center w-full">
+                                                    <Heading3 className="lg:text-4xl font-normal pb-8">
+                                                        {e.title}
+                                                    </Heading3>
+                                                    <Image src={e.img} alt="Tracer demo" className="rounded-2xl w-full drop-shadow-lg" />
+                                                    <Paragraph className="leading-loose">
+                                                        {e.text}
+                                                    </Paragraph>
+                                                </div>
+                                            </>
+                                        )
+                                }
+                            </div>
+                        )
+                    })
+                }
+            </Section>
             <Separator />
             <Section className="flex flex-row grow w-full justify-center">
-                <div className="flex flex-col w-3/5 rounded-2xl p-8 py-8 bg-secondary drop-shadow-lg">
+                <div className="flex flex-col w-11/12 lg:w-3/5 rounded-2xl p-8 py-8 bg-secondary drop-shadow-lg">
                     <Image src={kayfLogo} alt="logo" width={60} className={`absolute ${resolvedTheme == 'dark' ? 'invert' : ''}`} />
-                    <div className="flex flex-col px-8 py-20 justify-center items-center">
+                    <div className="flex flex-col px-0 lg:px-8 py-20 justify-center items-center">
                         <Heading3 className="text-center lg:text-5xl">
                             Built by the community
                             <br />
                             for the community
                         </Heading3>
-                        <Paragraph className="text-center w-3/5">
+                        <Paragraph className="text-center w-full lg:w-3/5">
                             This project is built for the community and will always remain free. Our mission is to simplify data management and make powerful tools accessible to all. As an open-source solution under GPL-3.0, it allows everyone to use, modify, and enhance the software.
                         </Paragraph>
                     </div>
