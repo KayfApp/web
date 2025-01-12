ab441383c8b9e6da5e419f72608dc476aaa70c28
pbakshi05 <pbakshi@student.tgm.ac.at>
2025-01-12 13:43:06 +0100

    Downlod: Kayf Download

--- src/app/(sales)/download/page.tsx
+++ src/app/(sales)/download/page.tsx
@@ -1,13 +1,37 @@
 import { Main, Section } from "@/components/layout";
-import { Heading1 } from "@/components/ui/typography";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Heading1, Small, } from "@/components/ui/typography";
+import Image, { StaticImageData } from "next/image"
+import Link from "next/link";
+
 
 export default function Download() {
     return (
         <Main>
             <Section>
                 <Heading1>
-                    ...
+                    Download Kayf
                 </Heading1>
+                <Small className="mt-4">
+                    Get Kayf on Mobil and Desktop
+                </Small>
+                <div className="mt-[5%] grid grid-cols-2 gap-8">
+                    {/* <Image src={""} alt="Mobile" width={100} height={200} /> */}
+                    <Skeleton className="h-[600px] w-[500px] rounded-xl" />
+                    <Skeleton className="h-[600px] w-[500px] rounded-xl" />
+                    <h1 className="text-4xl">For Mobile</h1>
+                    <h1 className="text-4xl">For Desktop</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                    <div className="grid grid-rows-2">
+                    <p><Link href={"#"} className="hover:underline underline-offset-4">Download for iOS</Link></p>
+                    <p><Link href={"#"} className="hover:underline underline-offset-4">Download for android</Link></p>
+                    </div>
+                    <div className="grid grid-rows-2">
+                    <p><Link href={"#"} className="hover:underline underline-offset-4">Download for macOS</Link></p>
+                    <p><Link href={"#"} className="hover:underline underline-offset-4">Download for Windows</Link></p>
+                    </div>
+                </div>
             </Section>
         </Main>
     )
