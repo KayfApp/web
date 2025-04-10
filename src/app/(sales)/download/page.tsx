f1ad182743691c90a1e732cd231a7074f02491b4
pbakshi05 <pbakshi@student.tgm.ac.at>
2025-04-11 00:37:52 +0200

    Bilder und Text auf der Downlaod Page ersetzt

--- src/app/(sales)/download/page.tsx
+++ src/app/(sales)/download/page.tsx
@@ -3,6 +3,11 @@ import { LinkHoverAnim } from "@/components/ui/link";
 import { Separator } from '@/components/ui/separator'
 import { Skeleton } from "@/components/ui/skeleton";
 import { Heading1, Heading2, Heading3, Large, Small, } from "@/components/ui/typography";
+import Image from 'next/image'
+import handy from "@/../public/download/handy.png"
+import laptop from "@/../public/download/laptop.png"
+
+
 
 export default function Download() {
     return (
@@ -12,25 +17,33 @@ export default function Download() {
                     Download Kayf
                 </Heading1>
                 <Small className="mt-4">
-                    Get Kayf on Mobile and Desktop
+                    Lade die Web-App von Kayf für ein besseres Erlebnis herunter
                 </Small>
 
                 <div className="mt-20 flex flex-col lg:flex-row space-y-20 lg:space-y-0 lg:space-x-20">
                     <div className="flex flex-col items-center lg:items-start w-[330px] md:w-[500px]">
-                        <Skeleton className="h-[400px] w-full md:h-[600px] rounded-xl" />
+                        <Image
+                            src={laptop}
+                            width={600}
+                            alt="Picture of the author"
+                        />
                         <div className="flex flex-col space-y-3 mt-4">
                             <Heading3>For Desktop</Heading3>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                            <p>Nutze Kayf direkt auf deinem Desktop, um deine Workflows effizient zu verwalten und Traces lokal zu analysieren.</p>
                             <LinkHoverAnim href={"#"}>Download for Windows</LinkHoverAnim>
                             <LinkHoverAnim href={"#"}>Download for Linux</LinkHoverAnim>
                         </div>
                     </div>
 
                     <div className="flex flex-col items-center lg:items-start w-[330px] md:w-[500px]">
-                        <Skeleton className="h-[400px] w-full md:h-[600px] rounded-xl" />
+                        <Image
+                            src={handy}
+                            width={500}
+                            alt="Picture of the author"
+                        />
                         <div className="flex flex-col space-y-3 mt-4">
                             <Heading3>For Mobile</Heading3>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                            <p>Bleibe auch unterwegs mit Kayf verbunden. Tracer überwachen, Workflows starten oder den Status prüfen – jederzeit verfügbar.</p>
                             <LinkHoverAnim href={"#"}>Download for iOS</LinkHoverAnim>
                             <LinkHoverAnim href={"#"}>Download for Android</LinkHoverAnim>
                         </div>
@@ -38,7 +51,6 @@ export default function Download() {
                 </div>
 
                 <Separator className="my-24" />
-
 
                 <Heading2>
                     Download Tracer
@@ -52,7 +64,7 @@ export default function Download() {
                         <Skeleton className="h-[400px] w-full md:h-[600px] rounded-xl" />
                         <div className="flex flex-col space-y-3 mt-4">
                             <Heading3>Docker Image</Heading3>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                            <p>Starte Tracer sofort mit dem vorkonfigurierten Docker Image – perfekt für schnelle Tests und CI/CD-Umgebungen.</p>
                             <LinkHoverAnim href={"#"}>From GitHub</LinkHoverAnim>
                             <LinkHoverAnim href={"#"}>From Docker Hub</LinkHoverAnim>
                         </div>
@@ -62,7 +74,7 @@ export default function Download() {
                         <Skeleton className="h-[400px] w-full md:h-[600px] rounded-xl" />
                         <div className="flex flex-col space-y-3 mt-4">
                             <Heading3>Source Code</Heading3>
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                            <p>Greife auf den Quellcode von Tracer zu – für volle Kontrolle, individuelle Anpassungen und Community-Entwicklung.</p>
                             <LinkHoverAnim href={"#"}>Get Latest Stable Release</LinkHoverAnim>
                             <LinkHoverAnim href={"#"}>Get Latest Development Version</LinkHoverAnim>
                         </div>
